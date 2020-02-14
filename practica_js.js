37ad85017cc5b74e9159fcf64ef7e13c3c3ad768
Jorge Rojas <jorgearojas25@gmail.com>
2020-02-13 19:04:24 -0500

    Ejercicios practicos dia martes

--- practica_js.js
+++ practica_js.js
@@ -77,29 +77,247 @@ const MensajeDiferencia = diferencia => {
 };
 
 const EjercicioAbc = () => {
-  let dato =  Number(prompt("Ingresa un numero"))
-  alert(dato <= 10 ? String.fromCharCode((dato + 96)) : "Este no es un valor aceptado")
-}
+  let dato = Number(prompt("Ingresa un numero"));
+  alert(
+    dato <= 10 ? String.fromCharCode(dato + 96) : "Este no es un valor aceptado"
+  );
+};
 
-let pistaV = [[false, false, false, false, false, false],
-              [false, true, true, true, true, false],
-              [false, true, false, false, true, false],
-              [false, true, false, false, true, false],
-              [false, true, true, true, true, false],
-              [false, false, false, false, false]]
+let pistaV = [
+  [false, false, false, false, false, false],
+  [false, true, true, true, true, false],
+  [false, true, false, false, true, false],
+  [false, true, false, false, true, false],
+  [false, true, true, true, true, false],
+  [false, false, false, false, false]
+];
 
 const EjericioJuegoVehiculos = (posicionx, posiciony, pista) => {
   console.log(pista);
-  let validacion = pista[posicionx][posiciony]
+  let validacion = pista[posicionx][posiciony];
   if (!validacion) {
     alert("Estas fuera de la pista");
-  }else{
+  } else {
     alert("Estas en la pista");
+  }
+};
+
+const EjecutarJuego = () => {
+  EjericioJuegoVehiculos(
+    Number(prompt("Ingresa la posicion en X")),
+    Number(prompt("Ingresa la posicion en y")),
+    pistaV
+  );
+};
+//EjecutarJuego();
+
+const user = "danialex15";
+const password = "123456";
+const ValidarIngreso = () => {
+  let insertUser = prompt("User: ");
+  let insertPassword = prompt("Password: ");
+  try {
+    if (user == insertUser && password == insertPassword) {
+      alert("Bienvenido: " + insertUser);
+    } else {
+      throw new Error("Los datos ingresados son incorrectos");
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+const PI = 3.1416;
+const EjercicioDiametro = () => {
+  let radio = prompt("Ingresa el radio del circulo: ");
+  try {
+    radio = parseFloat(radio);
+    if (isNaN(radio)) {
+      throw new Error("El valor ingrsado no es valido");
+    }
+    if (radio < 0) {
+      throw new Error("El numero ingresado no puede ser menor a 0");
+    }
+    alert("El diametro del circulo es: " + radio * 2 * PI);
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
+const Suma = () => {
+  let number1 = prompt("Ingresa un numero: ");
+  let number2 = prompt("Ingresa otro numero: ");
+  const result = 0;
+  try {
+    do {
+      number1 = parseFloat(number1);
+      number2 = parseFloat(number2);
+      if (isNaN(number1) || isNaN(number2)) {
+        throw new Error("Los datos ingresados no son validos");
+      }
+      result = number1 + number2;
+      return alert("El resultado es: " + result);
+    } while (result > 100);
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+const ARRAYSTOP = () => {
+  let dato = "";
+  let arrayDatos = [];
+  do {
+    dato = prompt("ingrese un numero | detengase con 'stop'").toLowerCase();
+    if (Number(dato)) {
+      arrayDatos.push(dato);
+    }
+  } while (dato !== "stop");
+  alert("los numeros que ingresaste fueron: \n" + arrayDatos);
+};
+
+const mostrarAlertas = () => {};
+
+const ArrayStopCero = () => {
+  let dato = "";
+  let arrayPares = [];
+  let arrayImpares = [];
+  let arrayNoNumeros = [];
+  do {
+    dato = prompt("ingrese un dato | detengase con '0'");
+    if (Number(dato)) {
+      if (dato % 2) {
+        if (dato !== 0) {
+          arrayImpares.push(dato);
+        }
+      } else {
+        arrayPares.push(dato);
+      }
+    } else {
+      if (dato !== "0" && dato !== "") {
+        arrayNoNumeros.push(dato);
+      }
+    }
+  } while (dato !== "0");
+  alert(
+    "Tus numeros pares fueron: " +
+      arrayPares +
+      "\n de longitud: " +
+      arrayPares.length
+  );
+  alert(
+    "Tus numeros impares fueron: " +
+      arrayImpares +
+      "\n de longitud: " +
+      arrayImpares.length
+  );
+  alert(
+    "Tambien ingresaste datos como: " +
+      arrayNoNumeros +
+      "\n de longitud: " +
+      arrayNoNumeros.length
+  );
+};
+
+const EjercicioEliminarExtremos = () => {
+  let dato = "";
+  let numeroMayor;
+  let numeroMenor;
+  let datos = [];
+  do {
+    dato = prompt("ingrese un dato | detengase con '0'");
+    if (Number(dato) && Number(dato) !== 0) {
+      datos.push(Number(dato));
+      console.log(`El dato es ${dato}`);
+      numeroMayor = numeroMayor > dato ? numeroMayor : dato;
+      numeroMenor = numeroMenor < dato ? numeroMenor : dato;
+      console.log(
+        `ahora numero mayor = ${numeroMayor} y numero menor = ${numeroMenor}`
+      );
+    }
+  } while (dato !== "0");
+  console.log(
+    `finalmente numero mayor = ${numeroMayor} y numero menor = ${numeroMenor}`
+  );
+  if (numeroMayor === numeroMenor) {
+    alert(
+      "El numero mayor y tambien el menor fue: " +
+        datos.splice(datos.indexOf(numeroMayor), 1)
+    );
+  } else {
+    console.log(datos);
+    console.log(datos.indexOf(numeroMayor));
+    console.log(datos.indexOf(numeroMenor));
+    alert(
+      "El numero mayor fue: " + datos.splice(datos.indexOf(numeroMayor), 1)
+    );
+    alert(
+      "El numero menor fue: " + datos.splice(datos.indexOf(numeroMenor), 1)
+    );
+  }
+  alert("El resto de datos fueron " + datos);
+};
+
+class usuario {
+  constructor(name, lastName, email, edad) {
+    (this.name = name),
+      (this.lastName = lastName),
+      (this.email = email),
+      (this.edad = edad);
+  }
+  isAdult = () => {
+    return Number(this.edad) > 18;
+  }
+  fullName = () => {
+    return `${this.name} ${this.lastName}`;
+  };
+}
+
+const createUser = () => {
+  let myUser = new usuario(
+    prompt("Ingresa tu nombre"),
+    prompt("Ingresa tu apellido"),
+    prompt("Ingresa tu email"),
+    prompt("Ingresa tu edad")
+  );
+  alert(
+    `Hola ${myUser.name} ${myUser.lastName} tu correo es: ${myUser.email} y tienes ${myUser.edad} años`
+  );
+  alert(
+    `Tu nombre completo es ${myUser.fullName()} y ${
+      myUser.isAdult() ? "Eres mayor de edad" : "No eres mayor de edad"
+    }`
+  );
+};
+
+class dog{
+  constructor(race, name, weight, age){
+    this.race = race,
+    this.name = name,
+    this.weight = weight,
+    this.age = age
   }
 }
 
-const EjecutarJuego = () => { 
-  EjericioJuegoVehiculos(Number(prompt("Ingresa la posicion en X")),Number(prompt("Ingresa la posicion en y")),pistaV);
+const myDogs = [];
+
+const getDataDog = (dog) => {
+  return console.log(`El perro de raza ${dog.race} se llama ${dog.name} pesa ${dog.weight}kg y tiene ${dog.age} años`);
 }
 
-//EjecutarJuego();
+const ejericioPerros = () => {
+  let race = '';
+  do {
+    race = prompt('Ingresa la raza del perro');
+    if(race.toLocaleLowerCase() !== 'stop'){
+      name = prompt('Ingresa el nombre del perro');
+      weight = prompt('Ingresa el peso del perro');
+      age = prompt('Ingresa la edad del perro');
+      myDogs.unshift(new dog(race, name, weight, age));
+    }
+
+  } while (race.toLocaleLowerCase() !== 'stop');
+  alert('Mira la consola');
+  myDogs.map(dog =>getDataDog(dog))
+}
+
+ejericioPerros();
